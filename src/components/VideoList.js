fa49cceb64b12996dd9d1f768b64c1415c6434a7
Ravindra Dahal <dahalravindra@gmail.com>
2021-02-10 16:35:51 +0545

    Defaulted video selection :sparkles:

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import VideoItem from './VideoItems';
 
-const VideoList = ({ videos,onVideoSelect }) => {
-	const renderedList = videos.map((video) => {
-		return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video}/>
-	});
+const VideoList = ({ videos, onVideoSelect }) => {
+  const renderedList = videos.map(video => {
+    return (
+      <VideoItem
+        key={video.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={video}
+      />
+    );
+  });
 
-	return <div className='ui relaxed divided list'>{ renderedList }</div>
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
